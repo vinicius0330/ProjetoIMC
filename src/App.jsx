e2c3439e350551528b700c7f79e7195d77ef16ac
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-08-21 09:46:26 -0300

    Criando a estrutura código do forms no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import Header from "./components/Header"
 
 function App() {
-
+ 
   return (
-    // fragments
-    <>  
-     
-    </>
+    // fragment
+    <div className="container">
+      <div className="box">
+     <Header/>
+     <form>
+      <div>
+        <label htmlFor="altura"><span className="span">(exemplo: 1.80)</span></label>
+        <input type="number" id="altura" placeholder="Digite sua Altura"
+        onBlur={({target})=>setAltura(parseFloat(target.value))}/>
+      </div>
+
+      <div>
+        <label htmlFor="peso"><span className="span">(exemplo: 80)</span></label>
+        <input type="number" id="peso" placeholder="Digite seu Peso"
+        onBlur={({target})=>setPeso(parseFloat(target.value))}/>
+      </div>
+
+      <button onClick={calcularImc}>Calcular</button>
+     </form>
+    </div>
+    {/* mostra o resultado ao digitar altura e peso */}
+    {mostraResultado && (
+
+      <Resultado resultado={resultado}/>
+    )}
+
+    </div>
+
   )
 }
 
