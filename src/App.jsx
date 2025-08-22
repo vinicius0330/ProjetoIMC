d15708eeef4a56c55ead310f8a3a8f938517ccf0
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-08-22 11:00:42 -0300

    Criando a função calcular e useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import {useState, useEffect} from "react"
 import Header from "./components/Header"
+import "./css/global.css"
+import "./css/estilo.css"
 
 function App() {
+
+  //HOOK-useState - manipula o estado da variável
+
+  const [peso,setPeso]=useState(0);
+  const [altura,setAltura]=useState(0);
+  const [resultado,setResultado]=useState(0);
+  const [mostrarresultado, setMostrarResultado]=useState(false);
+
+  // FUNÇÃO CALCULAR IMC
+  const calcularImc=()=>{
+    const imc = peso /(altura*altura)
+    return setResultado(imc.toFixed(2))
+  }
+  useEffect(()=>{
+    // ?-IF / :-ELSE
+    resultado > 0 ? setMostrarResultado(true) : setMostrarResultado(false)
+  },[resultado]) //Retorno
  
   return (
     // fragment
@@ -11,7 +31,8 @@ function App() {
       <div>
         <label htmlFor="altura"><span className="span">(exemplo: 1.80)</span></label>
         <input type="number" id="altura" placeholder="Digite sua Altura"
-        onBlur={({target})=>setAltura(parseFloat(target.value))}/>
+        // clicar fora ja funciona
+        onBlur={({target})=>setAltura(parseFloat(target.value))}/>    
       </div>
 
       <div>
@@ -24,7 +45,7 @@ function App() {
      </form>
     </div>
     {/* mostra o resultado ao digitar altura e peso */}
-    {mostraResultado && (
+    {mostrarresultado && (
 
       <Resultado resultado={resultado}/>
     )}
